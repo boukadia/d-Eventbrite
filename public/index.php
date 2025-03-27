52fdc9c8772c8e0bc615ac84f2ea09f8c1196b93
boukadia <mustapha.boukadia97@gmail.com>
2025-03-27 09:33:35 +0000

    affichage des evenements

--- public/index.php
+++ public/index.php
@@ -13,6 +13,7 @@ $routes = include __DIR__ . "/../Routes/api.php";
 $requestMethod = $_SERVER['REQUEST_METHOD'];
 $requestUri = $_SERVER['REQUEST_URI'];
 
+
 $routes->dispatch($requestMethod, $requestUri);
 
 
